2da28093612fec26dcb13c656e06f0acbbefcca3
olegsel4 <oleg_slesarenko@tut.by>
2017-04-19 13:21:37 +0300

    fix1

--- manifests/puppetagent.pp
+++ manifests/puppetagent.pp
@@ -1,21 +1,26 @@
-# Class install node1
-#
 class final_task::puppetagent ($version_agent = 'latest' ) {
 
-  package { 'puppet-repo':
-    ensure   => installed,
-    provider => rpm,
-    source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm'
+#  package { 'puppet-repo':
+#    ensure   => installed,
+#    provider => rpm,
+#    source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm'
+#  }
+  yumrepo { 'puppetlabs-pc1':
+    ensure   => 'present',
+    baseurl  => 'http://yum.puppetlabs.com/el/7/PC1/$basearch',
+    enabled  => '1',
   }
 
-  package { 'puppet-agent':
-    ensure  => $version_agent,
-    require => Package['puppet-repo'],
-  }
+  #package { 'puppet-agent':
+  #  ensure  => $version_agent,
+  #  require => Yumrepo['puppetlabs-pc1'],
+  #}
 
   service { 'puppet':
     ensure  => 'running',
-    require => Package['puppet-agent'],
+    require => Yumrepo['puppetlabs-pc1'],
   }
 }
+
+
 
