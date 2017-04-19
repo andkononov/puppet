f36e7264ec9a56f7f21681f30397eeefeb446c88
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-19 19:21:37 +0300

    t2

--- etc/puppetlabs/code/environments/production/modules/mcollective/manifests/init.pp
+++ etc/puppetlabs/code/environments/production/modules/mcollective/manifests/init.pp
@@ -15,13 +15,13 @@ class mcollective {
 
 }
 
-    define mcollective::setting ($setting = $title, $target = '/etc/mcollective/server.cfg', $value) {
-      validate_re($target, '\/(plugin\.d\/[a-z]+|server)\.cfg\Z')
-      $regex_escaped_setting = regsubst($setting, '\.', '\\.', 'G') # assume dots are the only regex-unsafe chars in a setting name.
+define mcollective::setting ($setting = $title, $target = '/etc/mcollective/server.cfg', $value) {
+  validate_re($target, '\/(plugin\.d\/[a-z]+|server)\.cfg\Z')
+  $regex_escaped_setting = regsubst($setting, '\.', '\\.', 'G') # assume dots are the only regex-unsafe chars in a setting name.
 
-      file_line {"mco_setting_${title}":
-        path  => $target,
-        line  => "${setting} = ${value}",
-        match => "^ *${regex_escaped_setting} *=.*$",
-      }
-    }
+  file_line {"mco_setting_${title}":
+    path  => $target,
+    line  => "${setting} = ${value}",
+    match => "^ *${regex_escaped_setting} *=.*$",
+  }
+}
