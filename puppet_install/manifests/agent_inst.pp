45053e29852f88d6acfac8eefb6a34cc3163f5be
Student <student@epbyminw2033.minsk.epam.com>
2017-04-19 14:18:51 +0300

    exam

--- puppet_install/manifests/agent_inst.pp
+++ puppet_install/manifests/agent_inst.pp
@@ -1,27 +1,36 @@
-class puppet_install::agent_inst  (
-  ) inherits puppet_install::params
+class puppet_install::agent_inst 
 
   {
   notify { 'Agent': }
 
   package { 'puppet-agent':
-    ensure  => installed,
+    ensure  => $::puppet_install::puppet_agent_ver,
     require => Yumrepo['puppet_repo'],
   }
 
   yumrepo { 'puppet_repo':
     ensure   => 'present',
-    baseurl  =>  "$baseurl",
-    gpgkey   =>  "$gpgkey",
+    baseurl  => $::puppet_install::baseurl,
+    gpgkey   => $::puppet_install::gpgkey,
     enabled  => '1',
     gpgcheck => '1',
+  }
+
+  exec {'puppet_conf':
+    command => 'echo "server = master.loc" >> /etc/puppetlabs/puppet/puppet.conf',
+    path => ['/usr/bin', '/usr/sbin',],    
+    require => Package['puppet-agent'],
   }
 
   service { 'puppet':
     ensure  => 'running',
     enable  => true,
-    require => Package['puppet-agent'],
+    require => Exec['puppet_conf'],
   }
+
+  include puppet_install::lighttpd
+
+   
   
 }
 
--- puppet_install/manifests/master_inst.pp
+++ puppet_install/manifests/master_inst.pp
@@ -1,24 +1,23 @@
-class puppet_install::master_inst (
-  ) inherits puppet_install::params
+class puppet_install::master_inst 
 
 {
   notify { 'Master': }
 
   package { 'puppetserver':
-    ensure  => installed,
+    ensure  => $::puppet_install::puppet_serv_ver,
     require => Yumrepo['puppet_repo'],
   }
 
   yumrepo { 'puppet_repo':
     ensure   => 'present',
-    baseurl  => "$baseurl",
-    gpgkey   => "$gpgkey",
+    baseurl  => $::puppet_install::baseurl,
+    gpgkey   => $::puppet_install::gpgkey,
     enabled  => '1',
     gpgcheck => '1',
   }
 
   file { 'autosign':
-    path    => "$autosign_path",
+    path    => $::puppet_install::autosign_path,
     content => template('puppet_install/autosign.erb'),
     require => Package['puppetserver'],
   }
