af3c5a2ca3270ca05788cf191651a2a0b9c37c7a
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-19 11:06:57 +0300

    rc1

--- node_provision.sh
+++ node_provision.sh
@@ -1,5 +1,5 @@
-yum install -y epel-release facter ruby wget unzip
-yum install -y puppet
+yum install -y epel-release facter ruby wget unzip > /dev/null 2>&1
+yum install -y puppet > /dev/null 2>&1
 
 grep -q -F '192.168.1.101 server.lab' /etc/hosts || echo '192.168.1.101 server.lab' >> /etc/hosts
 grep -q -F '192.168.1.102 node.lab' /etc/hosts || echo '192.168.1.102 node.lab' >> /etc/hosts
